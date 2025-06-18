f35d06f54c9732d48216bde85fe22db850ece7a0
kkorat10 <divya.shah@auction-cloud.com>
2025-06-18 16:59:35 +0530

    Added Advance Search filter in Role

--- LearnArchitecture.Core/Models/RequestModels/CreateRoleRequestModel.cs
+++ LearnArchitecture.Core/Models/RequestModels/CreateRoleRequestModel.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace LearnArchitecture.Core.Models.RequestModels
 {
-        public class CreateRoleRequestModel
-        {
-            public int roleId { get; set; }
-            public string roleName { get; set; }
-            public string description { get; set; }
-            public int[] pemissionids { get; set; }
-        }
+	public class CreateRoleRequestModel
+	{
+		public int roleId { get; set; }
+		public string roleName { get; set; }
+		public string description { get; set; }
+		public int[] pemissionids { get; set; }
+	}
+	public class RolePagingRequestModel
+	{
+		public int PageNumber { get; set; } = 1;
+		public int PageSize { get; set; } = 10;
+		public string? searchText { get; set; }
+		public string? SortColumn { get; set; }
+		public string? SortDirection { get; set; } // "asc" / "desc"
+	}
+
 }
--- LearnArchitecture.Services/IServices/IRoleService.cs
+++ LearnArchitecture.Services/IServices/IRoleService.cs
@@ -2,6 +2,7 @@
 using LearnArchitecture.Core.Helper;
 using LearnArchitecture.Core.Helper.Constants;
 using LearnArchitecture.Core.Models.RequestModels;
+using LearnArchitecture.Core.Models.ResponseModel;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -12,7 +13,7 @@ namespace LearnArchitecture.Services.IServices
 {
     public interface IRoleService
     {
-        public Task<ApiResponse<List<Role>>> GetAllRole(AuthClaim authClaim);
+        public Task<ApiResponse<PagingResponseModel<Role>>> GetAllRole(RolePagingRequestModel rolePagingRequestModel, AuthClaim authClaim);
         public Task<ApiResponse<Role>> GetRoleById(int roleId);
         //public Task<ApiResponse<bool>> CreateRole(CreateRoleRequestModel role,AuthClaim authClaim);
         public Task<ApiResponse<bool>> CreateRole(Role role, AuthClaim authClaim);
